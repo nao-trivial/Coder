cdba2775c687c28fa43f07198270afc9cc6c9112
Hassan Santos Primo <151403813+nao-trivial@users.noreply.github.com>
2024-10-09 20:52:41 -0300

    Atualizar o Argentina

---
+++ Solo Learn/Fácil/Argentina/argentina.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Program
+{
+    public static void main(String[] args) {
+    
+        Scanner input = new Scanner(System.in);
+        int pesos = input.nextInt();
+        int dollars = input.nextInt();
+        
+        double exchange = 0.02 * pesos;
+        
+        if (exchange > dollars) {
+            System.out.println("Dollars");
+        } else {
+            System.out.println("Pesos");
+        }
+    }
+}
