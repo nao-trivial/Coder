e49f3035c8c8b9da50755c396c84368329f9736e
Hassan Santos Primo <151403813+nao-trivial@users.noreply.github.com>
2024-10-13 20:51:06 -0300

    Atualizar o Halloween Candy

---
+++ Solo Learn/Fácil/Halloween Candy/halloween_candy.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Program
+{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int houses = input.nextInt();
+        double resultado;
+        //your code goes here
+        
+        resultado = (2.0 / houses) * 100;
+        double inteiro = Math.floor(resultado);
+        
+        if (resultado > inteiro) {
+        inteiro += 1;
+        System.out.println((int)inteiro);
+        } else {
+        System.out.println((int)inteiro);
+        }   
+    }
+}
+
