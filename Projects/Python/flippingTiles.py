aa93707d7a729def8aabf9b8341804e373291cc4
Hassan Santos Primo <151403813+nao-trivial@users.noreply.github.com>
2024-10-18 15:33:34 -0300

    Atualizar o flippingTiles.py

--- Projects/Python/flippingTiles.py
+++ Projects/Python/flippingTiles.py
@@ -17,3 +17,9 @@ def Square(x, y):
 
 def Numbering(x, y):
    return int((x + 200) // 50 + ((y + 200) // 50) * 8)
+
+# define function
+def Coordinates(count):
+   return (count % 8) * 50 - 200, (count // 8) * 50 - 200
+
+def click(
