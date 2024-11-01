003b113f6bb3a222e5c73c568900d247beaaf8dc
Hassan Santos Primo <151403813+nao-trivial@users.noreply.github.com>
2024-10-31 22:05:08 -0300

    Atualizar o day_of_week.py

--- Solo Learn/Médio/Dia da Semana/day_of_week.py
+++ Solo Learn/Médio/Dia da Semana/day_of_week.py
@@ -6,7 +6,7 @@ def get_day_of_week(date_str):
         date_obj = datetime.strptime(date_str, "%m/%d/%Y")
     except ValueError:
         try:
-            # If the first format fails, try "Month Day, Year" format
+            # Se o primeiro formato falhar, tente o formato "Month day, Year"
             date_obj = datetime.strptime(date_str, "%B %d, %Y")
         except ValueError:
             return "Invalid date format. Please use 'MM/DD/YYYY' or 'Month Day, Year'."
