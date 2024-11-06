f251535b36e30807dcead838741c29fbbca932a1
Hassan Santos Primo <hassanprimo500@gmail.com>
2024-11-05 22:01:58 -0300

    ATUALIZANDO

--- Exemplos/automator.py
+++ Exemplos/automator.py
@@ -1,6 +1,9 @@
 import pyautogui
 import time
 
+# Esperar 5 segundos para você se preparar
+time.sleep(5)
+
 # Coordenadas desejadas
 posicao = (1380, 275)
 # Mover o mouse para a posição desejada com uma duração de 3 segundos
@@ -40,3 +43,26 @@ pyautogui.write('919277', interval=0.1)
 
 # Pressionar Enter
 pyautogui.press('enter')
+
+# Coordenadas desejadas
+posicao = (102, 54)
+# Mover o mouse para a posição desejada com uma duração de 3 segundos
+pyautogui.moveTo(posicao, duration=3)
+pyautogui.click()
+
+pax = input("insira o nome do pax: ")
+
+# Coordenadas desejadas
+posicao = (93, 136)
+# Mover o mouse para a posição desejada com uma duração de 3 segundos
+pyautogui.moveTo(posicao, duration=3)
+pyautogui.click()
+
+# Digitar um texto
+pyautogui.write(pax, interval=0.1)
+
+# Coordenadas desejadas
+posicao = (1149, 244)
+# Mover o mouse para a posição desejada com uma duração de 3 segundos
+pyautogui.moveTo(posicao, duration=3)
+pyautogui.click()
