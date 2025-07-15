0dd6e5d740eb9247c7f1d25fa612f06fa3312b3d
Hassan Santos Primo <151403813+nao-trivial@users.noreply.github.com>
2025-07-14 23:00:14 -0300

    Criar halloween_candy.cpp

---
+++ Solo Learn/Fácil/Halloween Candy/halloween_candy.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int houses;
+    float resultado;
+    cin >> houses;
+
+    //your code goes here
+    
+    resultado = (2.0 / houses ) * 100;
+    
+    int inteiro = resultado;
+    
+    if (resultado > inteiro) {
+        inteiro += 1;
+        cout << inteiro;
+    } else {
+        cout << inteiro;
+    }  
+    
+    return 0;
+}
