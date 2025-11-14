d8b78d76fed7ac6867058fd9381a9b24d89780fb
Hassan Santos Primo <151403813+nao-trivial@users.noreply.github.com>
2025-11-14 14:31:34 -0300

    Atualizar o ponto_fixo_atrator.py

--- e-Book's/Teoria do Caos/iteracoes/ponto_fixo_atrator.py
+++ e-Book's/Teoria do Caos/iteracoes/ponto_fixo_atrator.py
@@ -1,8 +1,8 @@
 # Ponto Atrator - FÁCIL de observar
-def F(x):
+def f(x):
     return 0.5*x  # Ponto fixo atrator em x=0
 
 x = 0.1  # Não precisa ser exato!
 for i in range(10):
-    x = F(x)
+    x = f(x)
 print(x)  # Converge para 0 mesmo com erro inicial
