d2e48b81f036353ddf14eda50f61021d7e4df052
Hassan Santos Primo <151403813+nao-trivial@users.noreply.github.com>
2026-01-05 13:45:44 -0300

    Criar melhoria_continua.py

---
+++ e-Book's/3 Princípios para Aprender/melhoria_continua.py
@@ -0,0 +1,5 @@
+porcentagem = float(input("Quantos porcento você pretende se aprimorar hoje? "))
+
+minutos_necessarios = round(24 * 60 * (porcentagem / 100))
+
+print(f"Para se aprimorar {porcentagem}%, você precisa focar {minutos_necessarios} minutos hoje.")
